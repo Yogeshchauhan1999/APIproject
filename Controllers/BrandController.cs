96ddbeeddae6bc539a7c7eec02ba9126e0a5666d
Yogesh chauhan <yc19992023@gmail.com>
2023-10-05 10:30:04 +0530

    Firstcommit

---
+++ Controllers/BrandController.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using myWebAPIProject.Model;
+
+namespace myWebAPIProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandController : ControllerBase
+    {
+        private readonly BrandContext _context;
+        public BrandController(BrandContext context)
+        {
+            _context = context;
+
+        }
+
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<Brand>>> GetBrands()
+        {
+            if (_context.Brands == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return await _context.Brands.ToListAsync();
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Brand>> GetBrand(int id)
+        {
+            if (_context.Brands == null)
+            {
+                return NotFound();
+            }
+
+            var brand=await _context.Brands.FindAsync(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return brand;
+            
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Brand>> PostBrand(Brand brand)
+        {
+            _context.Brands.Add(brand);
+           await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(Brand), new {id=brand.ID},brand);
+        }
+
+        [HttpPut]
+
+        public async Task<IActionResult> PutBrand(int id, Brand brand)
+        {
+            if (id != brand.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(brand).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+                
+            }catch (DbUpdateConcurrencyException) {
+
+                if (!BrandAvailable(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return Ok();
+        }
+
+        private bool BrandAvailable(int id)
+        {
+            return (_context.Brands?.Any(x =>x.ID == id)).GetValueOrDefault();
+        }
+
+        [HttpDelete("{id}")]
+
+        public async Task<IActionResult> DeleteBrand(int id)
+        {
+            if (_context.Brands == null)
+            {
+                return NotFound();
+            }
+
+            var brand=await _context.Brands.FindAsync(id);
+
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            _context.Brands.Remove(brand);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ Model/BrandContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace myWebAPIProject.Model
+{
+    public class BrandContext:DbContext
+    {
+        public BrandContext(DbContextOptions<BrandContext> options):base(options)
+        {
+            
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+    }
+}
